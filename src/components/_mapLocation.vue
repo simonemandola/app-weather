232fdb2c478585a5b43d85814066b17418cf2c26
Simone <s.mandola.design@hotmail.com>
2022-02-26 01:27:04 +0100

    map location style update

--- src/components/_mapLocation.vue
+++ src/components/_mapLocation.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="map" ref="map">
+    <div class="marker" ref="marker">
+      <i
+        class="icon__location"
+        :class="{ 'text-grey': !isNightCurrentDate }"
+      ></i>
+    </div>
   </div>
 </template>
 
 <script>
 // mapbox
 import mapboxgl from "mapbox-gl";
+
+// mixins
+import { isNight } from "@/mixins/mixins.js";
 
 export default {
   name: "MapLocation",
@@ -15,19 +24,37 @@ export default {
       key: "pk.eyJ1Ijoic2ltb25lbWFuZG9sYSIsImEiOiJja3djYTY3cmkzc3dtMzByb2NnaXFqdGd3In0.LkO9Y9a2d2o50nNLp476eQ",
       zoom: 12,
       refMap: "",
+      refMarker: "",
       lat: this.$store.state.locationData[0].coord.lat,
       lon: this.$store.state.locationData[0].coord.lon,
+      mapStyle: {
+        light: "mapbox://styles/mapbox/light-v10",
+        dark: "mapbox://styles/mapbox/dark-v10",
+      },
     };
   },
+  computed: {
+    isNightCurrentDate() {
+      const currentDate = this.$store.state.locationData[0].current.dt;
+      const sunset = this.$store.state.locationData[0].current.sunset;
+      return isNight(currentDate, sunset);
+    },
+  },
   mounted() {
+    console.log(this.isNightCurrentDate);
     this.refMap = this.$refs.map;
+    this.refMarker = this.$refs.marker;
     mapboxgl.accessToken = this.key;
     this.map = new mapboxgl.Map({
-      container: this.refMap, // container ID
-      style: "mapbox://styles/mapbox/streets-v11", // style URL
+      container: this.refMap, // container ref
+      style: this.isNightCurrentDate ? this.mapStyle.dark : this.mapStyle.light, // style URL
       center: [this.lon, this.lat], // starting position [lng, lat]
       zoom: this.zoom, // starting zoom
     });
+    // Add marker to map
+    new mapboxgl.Marker(this.refMarker)
+      .setLngLat([this.lon, this.lat])
+      .addTo(this.map);
   },
 };
 </script>
---
+++ src/mixins/mixins.js
@@ -0,0 +1,19 @@
+// Mixins for VUE
+
+// let heightTransition = {
+//   methods: {
+//     beforeEnterHeightTransition: function (el) {
+//       el.style.height = "0";
+//     },
+//     enterHeightTransition: function (el) {
+//       el.style.height = `${el.scrollHeight}px`;
+//     },
+//     leaveHeightTransition: function (el) {
+//       el.style.height = "0";
+//     },
+//   },
+// };
+
+export const isNight = (date, sunsetDate) => {
+  return date < sunsetDate;
+}
